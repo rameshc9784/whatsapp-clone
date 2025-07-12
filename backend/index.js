9694c19eedc7474e6989a873dd6febbaf97af2ba
Ramesh Choudhary <rameshc9784@gmail.com>
2025-07-12 18:17:47 +0530

    database connection

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const connectDB = require('./config/dbConnect');
 
 dotenv.config();
 
 const port = process.env.PORT || 4000;
 
 const app = express();
+connectDB();
+
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
