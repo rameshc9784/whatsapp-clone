55a49d6e997c59fc8f02f2644bf7b7ed237624f3
Ramesh Choudhary <rameshc9784@gmail.com>
2025-07-12 17:49:03 +0530

    backend starter code

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const port = process.env.PORT || 4000;
+
+const app = express();
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
